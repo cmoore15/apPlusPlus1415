61e82e34354c235093710e0659a01e1d0170bdf8
cmoore15 <fawkes15@comcast.net>
2014-10-10 13:16:36 -0700

    tried to fix SFML

--- GameOfLife/GameOfLife/GameOfLife.cpp
+++ GameOfLife/GameOfLife/GameOfLife.cpp
@@ -4,12 +4,35 @@
 #include "stdafx.h"
 #include "Game.h"
 #include <iostream> 
+#include <SFML/Graphics.hpp>
+#include <string>
 using namespace std;
+using std::string;
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "How large would you like the grid? ";
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+	
+	/*cout << "How large would you like the grid? ";
 	int size;
 	cin >> size;
 	cout << endl;
@@ -20,6 +43,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	int c;
 	cin >> c;
 
-	return 0;
+	return 0;*/
 }
 
--- SmflExperiment/SmflExperiment/SmflExperiment.cpp
+++ SmflExperiment/SmflExperiment/SmflExperiment.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <SFML/Graphics.hpp>
 
+//TODO: look at http://stackoverflow.com/questions/3563756/fatal-error-lnk1112-module-machine-type-x64-conflicts-with-target-machine-typ
+//				http://sfml-dev.org/tutorials/2.1/start-vc.php 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
 
